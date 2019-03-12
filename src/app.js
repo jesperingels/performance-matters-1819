cf9068a809541f6f8e1e7d35c23e158b15628448
jesperingels <jesper.ingels@gmail.com>
2019-03-12 14:15:51 +0100

    Use ejs

---
+++ src/app.js
@@ -0,0 +1,157 @@
+/*******************************
+ * Create Modules
+ * Use Arrow Functions
+ */
+
+
+(() => {
+
+    const api = {
+        // Set base URL for dynamic link
+        solveUrl:(ID)=>{
+          return `https://swapi.co/api/${ID}`
+        }
+    };
+
+    const getData = {
+        people:async ()=>{
+            const a = await getData.checkExisting('people');
+            render.item(a,'people')
+        },
+
+        planets:async ()=> {
+            const a = await getData.checkExisting('planets');
+            render.item(a,'planets')
+        },
+
+        species:async ()=> {
+            const a = await getData.checkExisting('species');
+            render.item(a,'species')
+        },
+
+        starships:async ()=> {
+            const a = await getData.checkExisting('starships');
+            render.item(a,'starships')
+        },
+
+        // Set function to be asynchronous
+        checkExisting:async (string)=>{
+            // Get item from local storage
+            let data = window.localStorage.getItem(`swapi-${string}`);
+
+            // check if the item exists
+            if (!data) {
+                // Set data with URL from api.solveUrl
+                data = await getData.req(api.solveUrl(string));
+                // Save in localStorage as string
+                window.localStorage.setItem(`swapi-${string}`, JSON.stringify(data));
+                console.log('new');
+                return data;
+            } else {
+                console.log('existing');
+                return JSON.parse(data);
+            }
+        },
+
+        req: (url)=> {
+            console.log(url);
+            // Fetch and return data
+            return new Promise((resolve, reject) => {
+                fetch(url)
+                    .then(res => res.json())
+                    .then(res => resolve(res))
+                    .catch(error => reject(error))
+            })
+        }
+    };
+
+    const render = {
+        item: (data, id) => {
+            console.log(data);
+            navigation.prev();
+
+            document.querySelector('.wrapper').classList.add('d-none');
+            data.results.forEach( prop => {
+                const elWrapper = document.getElementById('content');
+                    const elPar = document.createElement("p");
+                    elPar.textContent = prop.name;
+                    elWrapper.appendChild(elPar);
+            });
+        }
+    };
+
+
+    // const navigation = {
+    //     init:()=>{
+    //         let navi = document.querySelectorAll('.wrapper > *');
+    //
+    //         // For each click on category element
+    //         navi.forEach(navEl =>{
+    //             navEl.addEventListener('click',() => {
+    //
+    //                 document.querySelector('.wrapper').classList.add('d-none');
+    //                 document.getElementById('content').classList.remove('d-none');
+    //                 document.getElementById('back').style.display = 'block';
+    //             })
+    //         });
+    //
+    //         const backButton = document.getElementById('back');
+    //         const dataContent = document.getElementById('content');
+    //
+    //         // Click on backbutton event
+    //         backButton.addEventListener('click', () => {
+    //             dataContent.classList.add('d-none');
+    //             document.querySelector('.wrapper').classList.remove('d-none');
+    //             backButton.style.display = 'none';
+    //         })
+    //     },
+    //
+    //     prev: () => {
+    //         const backButton = document.getElementById('back');
+    //         backButton.style.display = 'block';
+    //         const dataContent = document.getElementById('content');
+    //
+    //         // Template engine part
+    //         while (dataContent.firstChild) {
+    //             dataContent.removeChild(dataContent.firstChild);
+    //         }
+    //
+    //     }
+    // };
+    //
+    // navigation.init();
+
+    //     routie({
+    //     'people': function() {
+    //         getData.people();
+    //     },
+    //
+    //     'species': function() {
+    //         getData.species();
+    //     },
+    //
+    //     'starships': function() {
+    //         getData.starships();
+    //     },
+    //
+    //     'planets': function() {
+    //         getData.planets();
+    //     }
+    //
+    // });
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -5,25 +5,13 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
 app.get('/about', (req, res) => {
     res.render('pages/about');
 });
-
-
-app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
-    });
-});
-
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
