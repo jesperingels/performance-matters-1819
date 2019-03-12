5034f06b85e9622cd2f8b1c6b3dbb48c0ee9d90d
jesperingels <jesper.ingels@gmail.com>
2019-03-12 13:53:00 +0100

    Add template engine

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+let ejs = require('ejs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.set('view engine', 'ejs');
 
+app.get('/about', (req, res) => {
+    res.render('pages/about');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/', function(req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+});
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 app.use(express.static('public'));
+
