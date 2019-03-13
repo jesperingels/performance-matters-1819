1e7a9e2e5275fb8a9b8359a4149b297fbf35f8b3
jesperingels <jesper.ingels@gmail.com>
2019-03-13 09:43:30 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 let ejs = require('ejs');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
 app.set('view engine', 'ejs');
 
@@ -9,11 +10,47 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.get('/about', (req, res) => {
-    res.render('pages/about');
+app.get('/people', async (req, res) => {
+    // const peopleData = data.req(api.solveUrl('people'));
+    // const planetsData = data.req(api.solveUrl('planets'));
+    // const speciesData = data.req(api.solveUrl('species'));
+    // const starshipsData = data.req(api.solveUrl('starships'));
+
+    const peopleData = await data.req(api.solveUrl('people'));
+
+    res.render('pages/people', {
+        peopleData: peopleData.results
+    });
 });
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 app.use(express.static('public'));
+
+const api = {
+    // Set base URL for dynamic link
+    solveUrl:(ID)=>{
+        return `https://swapi.co/api/${ID}`
+    }
+};
+
+const data = {
+    req: (url)=> {
+        console.log(url);
+        // Fetch and return data
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(res => res.json())
+                .then(res => resolve(res))
+                .catch(error => reject(error))
+        })
+    }
+};
+
+
+
+
+
 
