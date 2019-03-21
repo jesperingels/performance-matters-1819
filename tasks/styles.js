9b95e1639120f3d1750d9ff1672f2e35a7ec3e32
jesperingels <jesper.ingels@gmail.com>
2019-03-21 11:21:57 +0100

    download bootstrap and minify

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
 
 gulp.src('public/css/*.css')
-.pipe(cssnano())
-.pipe(rename('style.min.css'))
-.pipe(gulp.dest('dist'));
+    .pipe(concat('all.css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist'));
